f8bea9826302ee23386112222187101d12cae588
attila5287 <atiturkoz@hotmail.com>
2020-03-19 03:56:51 -0600

    tree and static fx

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    FLASK_ENV=os.environ.get('FLASK_ENV')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- squares/__init__.py
+++ squares/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ENV'] = os.environ.get('FLASK_ENV')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)

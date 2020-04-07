2744c19d4ae80ddf61f7baf7905c8a0434193a23
attila5287 <atiturkoz@hotmail.com>
2020-04-07 01:37:26 -0600

    ddslick dynamic after js recap

---
+++ squares/templates/index.js
@@ -0,0 +1,26 @@
+var manuf_select = document.getElementById("manufacturer");
+var item_select = document.getElementById("item");
+
+manuf_select.onchange = function () {
+
+  manufacturer = manuf_select.value;
+
+  fetch('/manufacturer/' + manufacturer).then(function (response) {
+
+    response.json().then(function (data) {
+      var optionHTML = '';
+      for (var item of data.items) {
+        optionHTML +=
+          '<option value=" ' +
+          item.id + ' ">' +
+          item.catalog_no + '-' +
+          item.color_primary + '-' +
+          item.color_secondary +
+          '</option>';
+      }
+
+      item_select.innerHTML = optionHTML;
+    })
+
+  });
+}
